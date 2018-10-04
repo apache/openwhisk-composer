3254c718e1c3381cd216dc583e404068c3e54298
David Grove <groved@us.ibm.com>
2018-10-04 17:02:25 -0400

    put in bogus whitespace to get scancode to fail

--- travis/scancode.sh
+++ travis/scancode.sh
@@ -2,6 +2,8 @@
 # Licensed to the Apache Software Foundation (ASF) under one or more contributor
 # license agreements; and to You under the Apache License, Version 2.0.
 
+set -e   
+
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../"
 HOMEDIR="$SCRIPTDIR/../../"
