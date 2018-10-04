5c3fdd87730e65f438695a5d7e71987e085fafba
David Grove <groved@us.ibm.com>
2018-10-04 17:35:43 -0400

    remove trailing spaces (test worked)

--- travis/scancode.sh
+++ travis/scancode.sh
@@ -2,7 +2,7 @@
 # Licensed to the Apache Software Foundation (ASF) under one or more contributor
 # license agreements; and to You under the Apache License, Version 2.0.
 
-set -e   
+set -e
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../"
